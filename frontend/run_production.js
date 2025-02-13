ef7bc3a6c371e5273e7526623932d8806ee044d4
thomas_TableSurfer <tshort00011@gmail.com>
2025-02-13 09:11:17 -0600

    Updated and added comments that help explain what parts of the code are doing. Reformatted document for correct tab spacing.

--- frontend/run_production.js
+++ frontend/run_production.js
@@ -4,16 +4,17 @@ const express = require('express');
 const { name } = require('ejs');
 const app = express();
 
+//Set up for .ejs
 app.set('view engine', 'ejs');
 
 
-//Is needed to use POST 
+//Set up for app.post
 app.use(express.urlencoded({ extended: true }));
 
 // Path to connect to the database
 const dbPath = './DBeaver/Production/registration-sample-DB-Production.db';
 
-// Open the database
+// Opens the database
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
     console.error('Error opening database:', err.message);
@@ -22,7 +23,7 @@ const db = new sqlite3.Database(dbPath, (err) => {
   }
 });
 
-//Route to faculty dashboard, requires a unique key //STICH IN KRIS'S TIME VIEWS
+//Route to faculty dashboard, requires a unique key 
 app.get('/faculty_main/:fac_key', (req, res) => {
   //Query returns all information of students under a specific advisor
   const studentInfoSqlQuery =
@@ -49,36 +50,38 @@ app.get('/faculty_main/:fac_key', (req, res) => {
   //Runs validateFacultyQuery
   db.get(validateFacultyQuery, (err, fac_info) => {
     if (err) {
-      console.log("Error accessing database | validateFacultyQuery.")
+      console.log("Error accessing database => validateFacultyQuery.")
       return res.status(500).send('Error retrieving data from database');
     }
 
     if (fac_info) { // If validateFacultyQuery was successful...
       console.log(fac_info);
+      //Attempts to request student information from the database.
       db.all(studentInfoSqlQuery, [], (err, student_info) => {
         if (err) {
-          console.log("Error accessing database | studentInfoQuery.")
+          console.log("Error accessing database => studentInfoQuery.")
           return res.status(500).send('Error retrieving data from database');
         }
 
-        //same thing but for times
+        //Attempts to request meeting time information from the database.
         db.all(timeInfoQuery, [], (err, time_info) => {
           if (err) {
-            console.log("Error retrieving available meeting times | timeInfoQuery.");
+            console.log("Error retrieving available meeting times => timeInfoQuery.");
             return res.status(500).send('Error retrieving available times');
           }
 
-          if (student_info) { // If studentInfoSqlQuery was successful...
+          if (student_info && time_info) { // If studentInfoSqlQuery & timeInfoQuery was successful...
             // console.log(student_info);
             // This asks the .js to render views/faculty_view_main.ejs. 
             res.render('faculty_view_main', {
-              //Easier to read this way
+              //Values to pass into the .ejs.
               fac_info: fac_info,
               student_info: student_info,
               time_info: time_info
 
             });
           } else {
+            //If connnection to the database was unsuccessful, send the user to the invalid screen.
             res.render('invalid_key', { key: req.params.fac_key });
           }
         });
@@ -91,36 +94,43 @@ app.get('/faculty_main/:fac_key', (req, res) => {
   });
 });
 
-//Route for editing students page 
+//Route for editing students.
 app.get('/faculty_main/edit_student/:student_key', (req, res) => {
+  //Query gathers all information about the selected student from the database.
   const studentQuery =
     `SELECT * 
     FROM Persons
     WHERE Unique_Key IS "${req.params.student_key}"
     `;
 
+  //Attempts to request information for a specific student from the database.
   db.all(studentQuery, [], (err, student) => {
     if (err) {
       console.error('Error retrieving the student:', err);
       return res.status(500).send('Error retrieving the student');
     }
 
+    //Takes user to the edit_student.ejs view.
     res.render('edit_student', {
+      //Information passed to the .ejs.
       Student: student,
     });
   });
 });
 
-//Updates student information
+//Updates student's information.
 app.post('/update-student', (req, res) => {
   const { student_key, first_name, last_name, group, email, fac_key } = req.body;
 
+  //Checks the first name field for changes.
   if (first_name != "") {
+    //If changes are found, changes are put into query.
     changeFirstNameQuery =
       `UPDATE Persons
       SET First_Name = "${first_name}"
       WHERE Unique_Key IS "${student_key}"
       `;
+    //The query is run to attempt sending the update to the database.
     db.run(changeFirstNameQuery, [], function (err) {
       if (err) {
         console.error('Error changing name:', err.message);
@@ -128,12 +138,15 @@ app.post('/update-student', (req, res) => {
       }
     });
   }
+  //Checks the last name field for changes.
   if (last_name != "") {
+    //If changes are found, changes are put into query.
     changeLastNameQuery =
       `UPDATE Persons
       SET Last_Name = "${last_name}"
       WHERE Unique_Key IS "${student_key}"
     `;
+    //The query is run to attempt sending the update to the database.
     db.run(changeLastNameQuery, [], function (err) {
       if (err) {
         console.error('Error changing name:', err.message);
@@ -141,12 +154,15 @@ app.post('/update-student', (req, res) => {
       }
     });
   }
+  //Checks the group field for changes.
   if (group != "") {
+    //If changes are found, changes are put into query.
     changeGroupQuery =
       `UPDATE Persons
       SET "Group" = "${group}"
       WHERE Unique_Key IS "${student_key}"
       `;
+    //The query is run to attempt sending the update to the database.
     db.run(changeGroupQuery, [], function (err) {
       if (err) {
         console.error('Error changing classification:', err.message);
@@ -154,12 +170,15 @@ app.post('/update-student', (req, res) => {
       }
     });
   }
+  //Checks the email field for changes.
   if (email != "") {
+    //If changes are found, changes are put into query.
     changeEmailQuery =
       `UPDATE Persons
       SET Email = "${email}"
       WHERE Unique_Key IS "${student_key}"
       `;
+    //The query is run to attempt sending the update to the database.
     db.run(changeEmailQuery, [], function (err) {
       if (err) {
         console.error('Error changing email:', err.message);
@@ -167,44 +186,50 @@ app.post('/update-student', (req, res) => {
       }
     });
   }
+  //When the updates are finished, the user is sent back to faculty_main.ejs view.
   res.redirect('/faculty_main/' + fac_key);
 });
 
-//Editing Meeting Times
+//Route for editing meeting times.
 app.get('/faculty_main/edit_meeting_time/:fac_key/:date/:time', (req, res) => {
+  //Query gathers all information about the selected meeting time, where 
+  //the associated professor matches the requested time.
   const meetingQuery =
     `SELECT * FROM RegistrationList
     WHERE Professor_ID IS "${req.params.fac_key}" 
     AND Date_Available IS "${req.params.date}" 
     AND Time IS "${req.params.time}"
   `;
-
+  //Attempts to request information from the database.
   db.all(meetingQuery, [], (err, meeting) => {
     console.log(meeting);
     if (err) {
       console.error('Error retrieving the meeting:', err);
       return res.status(500).send('Error retrieving the meeting');
     }
-
+    //Checks if the query has any information
     if (meeting.length === 0) {
       return res.status(404).send('Meeting time not found');
     }
-
+    //Sends the user to the edit_meeting_time.ejs view.
     res.render('edit_meeting_time', {
+      //Information passed to the .ejs.
       Meeting: meeting
     });
   });
 });
 
-// Updating Meeting Times
+// Updates meeting times.
 app.post('/update-meeting', (req, res) => {
   const { fac_key, old_date, old_time, new_date, new_time } = req.body;
   console.log(req.body);
-  // Check if the user provided new values
+  // Checks if the user provided new values
   if (!new_date || !new_time) {
     return res.status(400).send('New date and time are required');
   }
-
+  //Query gathers the information from the existing meeting time, then 
+  //updates the old fields with the new inputs from the user for
+  //the associated professor.
   const updateQuery =
     `UPDATE RegistrationList
     SET Date_Available = "${new_date}", Time = "${new_time}"
@@ -213,16 +238,18 @@ app.post('/update-meeting', (req, res) => {
     AND Time IS "${old_time}"
   `;
   console.log(updateQuery, "should see something");
+  //Attempts to update the database.
   db.run(updateQuery, [], function (err) {
     if (err) {
       console.error('Error updating meeting time:', err.message);
       return res.status(500).send('Error updating meeting time');
     }
   });
+  //When the updates are finished, the user is sent back to faculty_main.ejs view.
   res.redirect(`/faculty_main/${fac_key}`);
 });
 
-//Route to student registration dashboard, requires a unique key
+//Route to student registration dashboard, requires a unique key | NEEDS WORK!!!
 app.get('/student_main/:student_key', (req, res) => {
   //Query returns the full name of the student who matches the input id.
   const studentSqlQuery =
