df2c04b2e7649f8c377ce645f779fc2d546127bd
Anderson <imbatman321123@gmail.com>
2025-12-10 17:10:49 -0600

    Fixed database connection issues and added stylesheet to faculty_view_main

--- frontend/server.js
+++ frontend/server.js
@@ -5,6 +5,8 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 // Set up for .ejs
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '..', 'views'));  
